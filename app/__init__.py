5bedf0257a79d1ac7a23cd31b3abe9f7647bc136
okumujustine <okumujustine01@gmail.com>
2020-08-07 18:59:07 +0300

    database structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.authentication.controllers import authentication
 from app.consultation.controllers import consultation
--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,39 +1,62 @@
 
 from app import db
 
-# Define a base model for other database tables to inherit
+
 class Base(db.Model):
 
-    __abstract__  = True
+    __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
-    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    id = db.Column(db.BigInteger, primary_key=True)
+    date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
-                                           onupdate=db.func.current_timestamp())
+                              onupdate=db.func.current_timestamp())
 
-# Define a User model
+
 class User(Base):
 
-    __tablename__ = 'auth_user'
+    __tablename__ = 'user'
 
-    # User Name
-    name    = db.Column(db.String(128),  nullable=False)
-
-    # Identification Data: email & password
-    email    = db.Column(db.String(128),  nullable=False,
-                                            unique=True)
+    country = db.Column(db.String(128),  nullable=False)
+    region = db.Column(db.String(128),  nullable=False)
+    district = db.Column(db.String(128),  nullable=False)
+    phone = db.Column(db.BigInteger(),  nullable=False)
+    name = db.Column(db.String(128),  nullable=False)
+    email = db.Column(db.String(128),  nullable=True, unique=True)
     password = db.Column(db.String(192),  nullable=False)
+    status = db.Column(db.SmallInteger, nullable=False)
 
-    # Authorisation Data: role & status
-    role     = db.Column(db.SmallInteger, nullable=False)
-    status   = db.Column(db.SmallInteger, nullable=False)
+    def __init__(self, country, region, district, phone, name, email, password, status):
 
-    # New instance instantiation procedure
-    def __init__(self, name, email, password):
-
-        self.name     = name
-        self.email    = email
+        self.country = country
+        self.region = region
+        self.district = district
+        self.phone = phone
+        self.name = name
+        self.email = email
         self.password = password
+        self.status = status
 
     def __repr__(self):
-        return '<User %r>' % (self.name)          
+        return '<User %r>' % (self.name)
+
+
+class Role(Base):
+
+    __tablename__ = 'role'
+
+    role = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, role):
+        self.role = role
+
+
+# class UserRole(Base):
+    
+#     __tablename__ = 'user_role'
+
+#     user_id = db.Column(ForeignKey('user.id'))
+#     role_id = db.Column(ForeignKey('role.id'))
+
+#     def __init__(self, user_id, role_id):
+#         self.user_id = user_id
+#         self.role_id = role_id
