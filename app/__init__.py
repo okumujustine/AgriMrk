3fda696bea06592908c765017c8ff1d377c01a89
User <user@Users-MacBook-3.lan>
2020-08-07 20:09:37 +0400

    refined databse migration and added manage py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 from app.authentication.controllers import authentication
 from app.consultation.controllers import consultation
--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
-# from flask_script import Manager
-# # from flask_migrate import Migrate, MigrateCommand
-# from app import db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import db, app
 
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
-# manager = Manager(app)
-# manager.add_command('db', MigrateCommand)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
